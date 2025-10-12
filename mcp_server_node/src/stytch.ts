a86e22d8c15c21c93f9cf9f3d02ded0d321f3490
Max Gerber <89937743+max-stytch@users.noreply.github.com>
2025-10-12 10:08:13 -0700

    chore: add keepalive agent to stytch client (#8)

--- mcp_server_node/src/stytch.ts
+++ mcp_server_node/src/stytch.ts
@@ -1,15 +1,22 @@
 import { Client } from 'stytch';
 import type { AuthInfo } from '@modelcontextprotocol/sdk/server/auth/types.js';
 import { config } from './config.ts';
+import undici from 'undici';
 
 let client: Client | null = null;
 
 function getClient(): Client {
   if (!client) {
+    const dispatcher = new undici.Agent({
+      keepAliveTimeout: 6e6, // 10 minutes in MS
+      keepAliveMaxTimeout: 6e6, // 10 minutes in MS
+    });
+
     client = new Client({
       project_id: config.STYTCH_PROJECT_ID,
       secret: config.STYTCH_PROJECT_SECRET,
       custom_base_url: config.STYTCH_DOMAIN,
+      dispatcher,
     });
   }
   return client;
